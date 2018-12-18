517792c8ec7567d65302e1659ef1b9670bab5a63
vipinpapnai <vipin.papnai@qandle.com>
2018-12-18 09:53:11 +0530

    add connect n disconect halndlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path = require('path');
-const publicPath = path.join(__dirname,'../public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname,'../public');
 const port  = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.get('/',(req,res) => {
-    res.send();
+io.on('connection',(socket)=>{
+    console.log('New User Connected');
+    socket.on('disconnect',(socket)=>{
+        console.log('User Disconnected');
+    });
+
 });
 
-app.listen(port,() => {
+
+
+server.listen(port,() => {
     console.log(`Started on port ${port}`);
 });
 
