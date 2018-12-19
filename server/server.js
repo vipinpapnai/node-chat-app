d95482f203aa65b9348eb558fac9c14950ae2686
vipinpapnai <vipin.papnai@qandle.com>
2018-12-19 10:25:07 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname,'../public');
 const port  = process.env.PORT || 3000;
@@ -24,26 +25,16 @@ io.on('connection',(socket)=>{
     //     createdAt: 123456
     // });
 
-    socket.emit('newMessage',{
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage',generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage',{
-        from: 'Admin',
-        text: 'A New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage',generateMessage('Admin','A new User Joined'));
 
     socket.on('createMessage',(message) => {  // listen to event createMessage emited by client
         console.log('createMessage',message);
         //broadcast the message to all connection
-        io.emit('newMessage',{  // io.emit emits the event to all connection
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        io.emit('newMessage', generateMessage(message.from,message.text)) // io.emit emits the event to all connection
+
+
 
         // socket.broadcast.emit('newMessage',{
         //     from: message.from,
