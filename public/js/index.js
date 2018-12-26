7daa13ee6cb3d9e9f25771864bff21e981d6d778
vipinpapnai <vipin.papnai@qandle.com>
2018-12-26 10:07:35 +0530

    Add geolocation support via api

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,16 @@ socket.on('newMessage',function(message){
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank" > : My Current Location</a>');
+
+    li.text(`${message.from}`);
+    a.attr('href',message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 // socket.emit('createMessage',{
 //     from: 'Vipin',
 //     text: 'Hello'
@@ -33,3 +43,19 @@ jQuery('#message-form').on('submit', function(e){
 
     })
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function(e){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported by your browser');
+    }
+    navigator.geolocation.getCurrentPosition(function(position){
+       socket.emit('createLocationMessage',{
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude
+       });
+    },function(){
+        alert('Unable to fetch location');
+    })
+});
+
--- server/utils/message.js
+++ server/utils/message.js
@@ -6,5 +6,12 @@ const generateMessage = (from, text) =>{
     }
 };
 
+const generateLocationMessage = (from, lat, long) => {
+    return {
+        from,
+        url:`https://www.google.com/maps?q=${lat},${long}`,
+        createdAt: new Date().getTime()
+    };
+};
 
-module.exports = { generateMessage }
+module.exports = { generateMessage, generateLocationMessage }
