6da101b8f2e62242e490a23b572f97db4be8328c
vipinpapnai <vipin.papnai@qandle.com>
2019-01-24 09:58:04 +0530

    add users class n update user list event

--- public/js/chat.js
+++ public/js/chat.js
@@ -20,10 +20,29 @@ var scrollToBottom = function(){
 
 socket.on('connect',function(){
     console.log('Connectd to server');
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function(err){
+        if(err){
+            alert(err);
+            window.location.href = '/';
+        }else{
+            console.log('No error');
+        }
+    });
+
 });
 
 socket.on('disconnect',function(){
     console.log('Disconnectd from server');
+});
+
+socket.on('updateUserList',function(users){
+   var ol = jQuery('<ol></ol>');
+    users.forEach(function(user){
+        ol.append(jQuery('<li></li>').text(user));
+    })
+    jQuery('#users').html(ol);
 });
 
 socket.on('newMessage',function(message){
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const http = require('http');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/user');
 const express = require('express');
 const socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
@@ -10,24 +12,26 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+var users = new Users();
 app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
     console.log('New User Connected');
 
-    socket.on('disconnect',(socket)=>{
-        console.log('User Disconnected');
+    socket.on('join',(params,callback) => {
+       if(!isRealString(params.name) || !isRealString(params.room)){
+          return callback('Name and room name are required');
+       }
+
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+        io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+        socket.emit('newMessage',generateMessage('Admin', 'Welcome to the chat app'));
+        socket.broadcast.to(params.room) .emit('newMessage',generateMessage('Admin',`${params.name} has joined`));
+
+        callback();
     });
-
-    // socket.emit('newMessage',{   // socket.emit emits the event to single connection
-    //     from : 'vipin',
-    //     text : 'Hey, Whats up.',
-    //     createdAt: 123456
-    // });
-
-    socket.emit('newMessage',generateMessage('Admin', 'Welcome to the chat app'));
-
-    socket.broadcast.emit('newMessage',generateMessage('Admin','A new User Joined'));
 
     socket.on('createMessage',(message,callback) => {  // listen to event createMessage emited by client
         console.log('createMessage',message);
@@ -47,6 +51,13 @@ io.on('connection',(socket)=>{
         io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude, coords.longitude))
     });
 
+    socket.on('disconnect',()=>{
+        var user = users.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left`));
+            io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+        }
+    });
 });
 
 
